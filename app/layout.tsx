6d0d96763b5e84792ae373f3a0a706690d7dc7e5
ashabul jannat alif <you@example.com>
2023-10-07 19:36:22 +0600

    code push

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,14 @@ import GA from '@/components/GA';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Modifio - Free Unlimited File Converter',
-  description: `Unleash your creativity with Modifio – the ultimate online tool for
+  title: 'Free Unlimited File Converter',
+  description: `Unleash your creativity with Converter – the ultimate online tool for
   unlimited and free multimedia conversion. Transform images, audio, and
   videos effortlessly, without restrictions. Start converting now and
   elevate your content like never before!`,
-  creator: 'SOUHAIL BEN-LHACHEMI',
+
   keywords:
-    'image converter, video converter, audio converter, unlimited image converter, unlimited video converter',
+    'image converter, video converter, audio converter',
 };
 
 export default function RootLayout({
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,55 +1,47 @@
 // imports
-import Image from 'next/image';
-import { Button } from './ui/button';
-import Link from 'next/link';
-import { BsGithub } from 'react-icons/bs';
-import { GrMenu } from 'react-icons/gr';
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+import { GrMenu } from "react-icons/gr";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
   SheetHeader,
-  SheetTitle,
   SheetTrigger,
-} from '@/components/ui/sheet';
+} from "@/components/ui/sheet";
+import { LuFileAudio, LuLogIn } from "react-icons/lu";
 
-export default function Navbar({}): any {
+export default function Navbar(): any {
   return (
     <nav className="w-full backdrop-blur-md bg-white bg-opacity-30 z-50 fixed h-24 flex justify-between items-center py-10 px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-24">
       <Link href="/">
-        <Image
-          alt="logo"
-          className="cursor-pointer w-40"
-          src="/images/logo.svg"
-          height={100}
-          width={170}
-        />
+        <LuFileAudio size="30" />
       </Link>
       <div className="gap-1 md:gap-2 lg:gap-4 hidden md:flex">
         <Button variant="ghost" className="font-semibold text-md">
           <Link href="/">Home</Link>
         </Button>
-        <Link href="/about">
+        <Link href="/">
           <Button variant="ghost" className="font-semibold text-md">
             About
           </Button>
         </Link>
-        <Link href="/privacy-policy">
+        <Link href="/">
           <Button variant="ghost" className="font-semibold text-md">
             Privacy Policy
           </Button>
         </Link>
       </div>
-      <Link href="https://github.com/benlhachemi/modifio.git">
+      <Link href="/">
         <Button
           variant="default"
           className="rounded-full w-fit bg-orange-600 gap-2 items-center hidden md:flex"
-          size="lg"
+          size="sm"
         >
-          <span>Github Repo</span>
-          <span className="text-xl">
-            <BsGithub />
-          </span>
+          <span>Login</span>
+          <LuLogIn size="17" />
         </Button>
       </Link>
 
@@ -72,7 +64,7 @@ export default function Navbar({}): any {
                     Home
                   </Button>
                 </Link>
-                <Link href="/about">
+                <Link href="/">
                   <Button
                     variant="link"
                     className="font-semibold text-md w-full"
@@ -80,7 +72,7 @@ export default function Navbar({}): any {
                     About
                   </Button>
                 </Link>
-                <Link href="/privacy-policy">
+                <Link href="/">
                   <Button
                     variant="link"
                     className="font-semibold text-md w-full"
